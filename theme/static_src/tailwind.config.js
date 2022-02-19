6cc9f0edbb15af51032c0dab6634e4a1fff379ff
Owen Jones <owenmjones1@gmail.com>
2022-02-19 16:52:40 +0100

    Install daisyui and test daisyui & tailwind functionality

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -54,5 +54,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require("daisyui"),
     ],
 }
